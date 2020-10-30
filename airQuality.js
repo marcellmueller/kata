638fb8281dcb3eb1afe4cfc2d4b5172c97035fa2
marcellmueller <mlmueller@protonmail.com>
2020-10-29 19:07:20 -0700

    katas complete

--- airQuality.js
+++ airQuality.js
@@ -1,38 +1,38 @@
 const checkAir = (samples, threshold) => {
-	let dirty = 0;
-	for (let i = 0; i < samples.length; i++) {
-		if (samples[i] === "dirty") {
-			dirty++;
-		}
-	}
-	let airQuality = dirty / samples.length;
-	if (airQuality > threshold) {
-		return "Polluted";
-	} else {
-		return "Clean";
-	}
+  let dirty = 0;
+  for (const each of samples) {
+    if (each === "dirty") {
+      dirty++;
+    }
+  }
+  let airQuality = dirty / samples.length;
+  if (airQuality > threshold) {
+    return "Polluted";
+  } else {
+    return "Clean";
+  }
 };
 
 console.log(
-	checkAir(
-		[
-			"clean",
-			"clean",
-			"dirty",
-			"clean",
-			"dirty",
-			"clean",
-			"clean",
-			"dirty",
-			"clean",
-			"dirty",
-		],
-		0.3
-	)
+  checkAir(
+    [
+      "clean",
+      "clean",
+      "dirty",
+      "clean",
+      "dirty",
+      "clean",
+      "clean",
+      "dirty",
+      "clean",
+      "dirty",
+    ],
+    0.3
+  )
 );
 
 console.log(checkAir(["dirty", "dirty", "dirty", "dirty", "clean"], 0.25));
 
 console.log(
-	checkAir(["clean", "dirty", "clean", "dirty", "clean", "dirty", "clean"], 0.9)
+  checkAir(["clean", "dirty", "clean", "dirty", "clean", "dirty", "clean"], 0.9)
 );
--- bouncyCastles.js
+++ bouncyCastles.js
@@ -2,52 +2,52 @@
 const PI = 3.14159;
 
 const sphereVolume = (radius) => {
-	return (4 / 3) * PI * (radius * radius * radius);
+  return (4 / 3) * PI * (radius * radius * radius);
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
 const coneVolume = (radius, height) => {
-	return (1 / 3) * PI * (radius * radius) * height;
+  return (1 / 3) * PI * (radius * radius) * height;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = (height, width, depth) => {
-	return depth * width * height;
+  return depth * width * height;
 };
 
 console.log(prismVolume(3, 4, 5) === 60);
 
 const totalVolume = (solids) => {
-	let total = 0;
-	for (let i = 0; i < solids.length; i++) {
-		if (solids[i].type === "sphere") {
-			total = total + sphereVolume(solids[i].radius);
-		} else if (solids[i].type === "cone") {
-			total = total + coneVolume(solids[i].radius, solids[i].height);
-		} else if (solids[i].type === "prism") {
-			total =
-				total + prismVolume(solids[i].height, solids[i].width, solids[i].depth);
-		}
-	}
-	return total;
+  let total = 0;
+  for (let i = 0; i < solids.length; i++) {
+    if (solids[i].type === "sphere") {
+      total = total + sphereVolume(solids[i].radius);
+    } else if (solids[i].type === "cone") {
+      total = total + coneVolume(solids[i].radius, solids[i].height);
+    } else if (solids[i].type === "prism") {
+      total =
+        total + prismVolume(solids[i].height, solids[i].width, solids[i].depth);
+    }
+  }
+  return total;
 };
 
 const largeSphere = {
-	type: "sphere",
-	radius: 40,
+  type: "sphere",
+  radius: 40,
 };
 
 const smallSphere = {
-	type: "sphere",
-	radius: 10,
+  type: "sphere",
+  radius: 10,
 };
 
 const cone = {
-	type: "cone",
-	radius: 3,
-	height: 5,
+  type: "cone",
+  radius: 3,
+  height: 5,
 };
 
 const duck = [largeSphere, smallSphere, cone];
