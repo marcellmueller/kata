9a2d4a8966bf128a4d54a57215d41ff37e64b08a
marcellmueller <mlmueller@protonmail.com>
2020-10-31 17:43:22 -0700

    update

--- encodedString.js
+++ encodedString.js
@@ -1,16 +1,17 @@
 const urlEncode = (text) => {
-	let encoded = "";
-	for (let i = 0; i < text.length; i++) {
-		if (text[i] === " " && (i === 0 || i === text.length - 1)) {
-			encoded = encoded + "";
-		} else if (text[i] === " ") {
-			encoded = encoded + "%20";
-		} else {
-			encoded = encoded + text[i];
-		}
-	}
-	return encoded;
+  let encoded = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === " " && (i === 0 || i === text.length - 1)) {
+      encoded = encoded + "";
+    } else if (text[i] === " ") {
+      encoded = encoded + "%20";
+    } else {
+      encoded = encoded + text[i];
+    }
+  }
+  return encoded;
 };
+
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
